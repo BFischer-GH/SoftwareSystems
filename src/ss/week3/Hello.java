a181a13588d6f12a307cf6c410be35a9619b97df
BFischer-GH <twentescience@gmail.com>
2021-12-01 16:35:26 +0100

    This is ex D3-3, commit 2

--- src/ss/week3/Hello.java
+++ src/ss/week3/Hello.java
@@ -2,6 +2,6 @@ package ss.week3;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World, It's Time For Beer!");
+        //Now this is an empty Class
     }
 }
