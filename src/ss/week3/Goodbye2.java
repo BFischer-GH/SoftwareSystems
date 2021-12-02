2d3fcc0ccf93bcce6cad632bad56a69eb7dfcd8f
BFischer-GH <twentescience@gmail.com>
2021-12-02 09:28:29 +0100

    This is ex D3-6, commit new class to branch

---
+++ src/ss/week3/Goodbye2.java
@@ -0,0 +1,7 @@
+package ss.week3;
+
+public class Goodbye2 {
+    public static void main(String[] args) {
+        System.out.println("Goodbye world ");
+    }
+}
--- src/ss/week3/Hello.java
+++ src/ss/week3/Hello.java
@@ -3,5 +3,6 @@ package ss.week3;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("The method is changed!");
     }
 }
